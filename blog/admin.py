b554cfa3883309ef21c006c5007f7b4ccfcd8ee2
daniil-torubara <dabarara@bk.ru>
2022-10-24 12:59:01 +0300

    first commit

---
+++ blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from blog.models import Blog
+from django.contrib import admin
+
+# Register your models here.
+
+admin.site.register(Blog)
---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length = 50)
+    description = models.TextField()
+    date = models.DateField()
+    url = models.URLField(blank = True)
+
+    def __str__(self):
+        return self.title
+    
